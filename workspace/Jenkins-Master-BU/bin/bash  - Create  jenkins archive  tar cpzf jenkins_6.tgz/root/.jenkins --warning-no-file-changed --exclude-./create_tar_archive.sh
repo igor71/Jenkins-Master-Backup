cf5aba58f84af5264b0369066ff1751aefa56a71
igor71 <34439207+igor71@users.noreply.github.com>
2018-04-22 09:39:02 +0300

    Delete create_tar_archive.sh

--- workspace/Jenkins-Master-BU/bin/bash  - Create  jenkins archive  tar cpzf jenkins_6.tgz/root/.jenkins --warning-no-file-changed --exclude-./create_tar_archive.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-# Create  jenkins archive
-
-tar cpzf jenkins_6.tgz /root/.jenkins --warning=no-file-changed --exclude="./workspace" || ( export ret=$?; [[ $ret -eq 1 ]] || exit "$ret" )
