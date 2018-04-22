3780b038098b29b480db5be79830af3ee19c5573
igor71 <34439207+igor71@users.noreply.github.com>
2018-04-22 09:41:31 +0300

    Update create_tar_archive.sh

--- workspace/Jenkins-Master-BU/create_tar_archive.sh
+++ workspace/Jenkins-Master-BU/create_tar_archive.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
-# Create  jenkins archive
+# Create  jenkins archive file
+# When needed to ignore only exit status 1 but to preserve the exit status if it is anything else add folowing: 
+# || ( export ret=$?; [[ $ret -eq 1 ]] || exit "$ret" )
+# suppress these warning message "tar...: file changed as we read it" by setting option --warning=no-file-changed 
 
 tar cpzf jenkins_6.tgz /root/.jenkins --warning=no-file-changed --exclude="./workspace" || ( export ret=$?; [[ $ret -eq 1 ]] || exit "$ret" )
